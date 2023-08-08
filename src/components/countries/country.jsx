c6565f8cfcbaf41fcc7aa2afb579ecb67fc96b9b
agwadan <agwa.dan@gmail.com>
2023-08-08 20:16:20 +0300

    Update styling and add thousand separator

--- src/components/countries/country.jsx
+++ src/components/countries/country.jsx
@@ -6,10 +6,10 @@ const Country = ({name, flag, region, population, capital}) => {
     <div className='country'>
       <Link to={`/country/${name}`}>
         <img src={flag} />
-        <h3>Name: {name}</h3>
-        <p>Population: {population}</p>
-        <p>Region: {region}</p>
-        <p>Capital: {capital}</p>
+        <h3><strong>Name:</strong> {name}</h3>
+        <p><strong>Population:</strong> {population.toLocaleString('en-US')}</p>
+        <p><strong>Region:</strong> {region}</p>
+        <p><strong>Capital:</strong> {capital}</p>
       </Link>
     </div>
   )
