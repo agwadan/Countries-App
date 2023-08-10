ca8bbf1bca447d0928463ae338ceb04552969d6a
agwadan <agwa.dan@gmail.com>
2023-08-10 14:47:59 +0300

    Add icons for country card items

--- src/components/countries/country.jsx
+++ src/components/countries/country.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PopulationDark from '../../assets/population-dark.svg';
+import Region from '../../assets/region.svg';
+import Capital from '../../assets/capital.svg';
 
 const Country = ({name, flag, region, population, capital}) => {
   return (
     <div className='country'>
       <Link to={`/country/${name}`}>
         <img src={flag} />
-        <h3><strong>Name:</strong> {name}</h3>
-        <p><strong>Population: </strong> {population.toLocaleString('en-US')}</p>
-        <p><strong>Region:</strong> {region}</p>
-        <p><strong>Capital:</strong> {capital}</p>
+        <h3><strong>Name:&nbsp;</strong> {name}</h3>
+        <div className="item">
+          <img src={PopulationDark}/>
+          <p><strong>Population:&nbsp; </strong> <i>{population.toLocaleString('en-US')}</i></p>
+        </div>
+        <div className="item">
+        <img src={Region}/>
+        <p><strong>Region:&nbsp;</strong> <i>{region}</i></p>
+        </div>
+        <div className="item">
+        <img src={Capital}/>
+        <p><strong>Capital:&nbsp;</strong> <i>{capital}</i></p>
+        </div>
       </Link>
     </div>
   )
